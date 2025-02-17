3154e250863bc6295a419cf179465ef76adc2d0c
FrancineMette <francineisabelsantos@hotmaial.com>
2025-02-17 16:29:17 -0300

    Teste

---
+++ Exercício C# 1/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+
+    static void Main()
+    {
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+           
+        string mensagem = $"Olá, {nome}! Seja muito bem-vindo!";
+        
+        Console.WriteLine(mensagem);
+     }
+}
